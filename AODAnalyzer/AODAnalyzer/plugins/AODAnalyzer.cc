1d8db0fdd19757679367bddac1b593ce615285c1
Artem Bolshov <abolshov@cern.ch>
2024-03-12 03:00:26 +0100

    add FindStableParticles

--- AODAnalyzer/AODAnalyzer/plugins/AODAnalyzer.cc
+++ AODAnalyzer/AODAnalyzer/plugins/AODAnalyzer.cc
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <fstream>
+#include <list>
 
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
@@ -17,7 +19,8 @@
 #include "DataFormats/HepMCCandidate/interface/GenParticle.h"
 
 #include "TLorentzVector.h"
-static constexpr double PI = 3.14159;
+#include "TH1.h"
+
 
 class AODAnalyzer : public edm::one::EDAnalyzer<> 
 {
@@ -36,38 +39,48 @@ class AODAnalyzer : public edm::one::EDAnalyzer<>
 
 		enum class GENPART { H, h_bb, h_WW, b, bbar, Whad, Wlep, q1, q2, l, nu, Count };
 		enum class GENJET { b, bbar, q1, q2, Count }; // either make enum class or do something else
+		static std::ofstream m_file;
 		std::vector<reco::Candidate*> m_signal;
 		std::vector<reco::Candidate*> m_matched_jets;
+		double m_dr_thresh = 0.4;
+
+		// std::vector<TLorentzVector> m_stable_particles;
 
 		inline bool ValidSignal() const noexcept { return std::none_of(m_signal.begin(), m_signal.end(), [](reco::Candidate const* p) { return p == nullptr; }); }
 		bool FindSignal(reco::GenParticleCollection const& genpart);
 		int Match(GENPART target, std::vector<reco::GenJet> const& genjets);
+		std::list<TLorentzVector> FindStableParticles(reco::GenParticleCollection const& genpart);
+		std::vector<TLorentzVector> ClusterAK4(std::vector<TLorentzVector> stable);
 };
+
+std::ofstream AODAnalyzer::m_file("debug.csv");
 
 AODAnalyzer::AODAnalyzer(const edm::ParameterSet& iConfig)
     : m_genpart_token(consumes<reco::GenParticleCollection>(iConfig.getUntrackedParameter<edm::InputTag>("genparticles"))),
 	  m_genjet_token(consumes<std::vector<reco::GenJet>>(iConfig.getUntrackedParameter<edm::InputTag>("genjets"))),
 	  m_signal(static_cast<int>(GENPART::Count), nullptr),
-	  m_matched_jets(static_cast<int>(GENJET::Count), nullptr) {}
+	  m_matched_jets(static_cast<int>(GENJET::Count), nullptr),
+	  m_dr_thresh(iConfig.getUntrackedParameter<double>("dr_thresh")) {}
 
 
 AODAnalyzer::~AODAnalyzer() 
 {
-	if (!m_signal.empty())
+	for (auto& p_cand: m_signal)
 	{
-		for (auto& p_cand: m_signal)
-		{
-			if (p_cand) delete p_cand;
-		}
+		delete p_cand;
 	}
 
-	if (!m_matched_jets.empty())
+	
+	for (auto& p_jet: m_matched_jets)
 	{
-		for (auto& p_jet: m_matched_jets)
-		{
-			if (p_jet) delete p_jet;
-		}
+		delete p_jet;
 	}
+
+	// for (auto& p_stable: m_stable_particles)
+	// {
+	// 	delete p_stable;
+	// }
+	
 }
 
 
@@ -86,24 +99,147 @@ void AODAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
 	if (FindSignal(genpart))
 	{
 		std::cout << "\tFound signal in collection of size " << genpart.size() << "\n";
-		for (auto const pcand: m_signal)
+		std::cout << "\tHeavy higgs Mass = " << m_signal[static_cast<int>(GENPART::H)]->mass() << "\n";
+
+		int q1_match = Match(GENPART::q1, genjet);
+		int q2_match = Match(GENPART::q2, genjet);
+
+		int q1_idx = static_cast<int>(GENPART::q1);
+		reco::Candidate const* ptr_q1 = m_signal[q1_idx];
+		TLorentzVector q1(ptr_q1->px(), ptr_q1->py(), ptr_q1->pz(), ptr_q1->energy());
+
+		int q2_idx = static_cast<int>(GENPART::q2);
+		reco::Candidate const* ptr_q2 = m_signal[q2_idx];
+		TLorentzVector q2(ptr_q2->px(), ptr_q2->py(), ptr_q2->pz(), ptr_q2->energy());
+
+		int w_had_idx = static_cast<int>(GENPART::Whad);
+		reco::Candidate const* ptr_Whad = m_signal[w_had_idx];
+		TLorentzVector w_had(ptr_Whad->px(), ptr_Whad->py(), ptr_Whad->pz(), ptr_Whad->energy());
+
+		int w_lep_idx = static_cast<int>(GENPART::Wlep);
+		reco::Candidate const* ptr_Wlep = m_signal[w_lep_idx];
+		TLorentzVector w_lep(ptr_Wlep->px(), ptr_Wlep->py(), ptr_Wlep->pz(), ptr_Wlep->energy());
+
+		int lep_idx = static_cast<int>(GENPART::l);
+		reco::Candidate const* ptr_lep = m_signal[lep_idx];
+		TLorentzVector lep(ptr_lep->px(), ptr_lep->py(), ptr_lep->pz(), ptr_lep->energy());
+
+		int nu_idx = static_cast<int>(GENPART::nu);
+		reco::Candidate const* ptr_nu = m_signal[nu_idx];
+		TLorentzVector nu(ptr_nu->px(), ptr_nu->py(), ptr_nu->pz(), ptr_nu->energy());
+
+		int b_idx = static_cast<int>(GENPART::b);
+		reco::Candidate const* ptr_b = m_signal[b_idx];
+		TLorentzVector b(ptr_b->px(), ptr_b->py(), ptr_b->pz(), ptr_b->energy());
+
+		int bbar_idx = static_cast<int>(GENPART::bbar);
+		reco::Candidate const* ptr_bbar = m_signal[bbar_idx];
+		TLorentzVector bbar(ptr_bbar->px(), ptr_bbar->py(), ptr_bbar->pz(), ptr_bbar->energy());
+
+		double quark_1_cone = 0;
+		for (auto const& part: genpart)
 		{
-			if (pcand)
+			if (part.numberOfDaughters() == 0)
 			{
-				std::cout << "\t" << pcand->pdgId() << "\n";
-			}
-			else
-			{
-				std::cout << "\tnullptr\n";
+				TLorentzVector p4part(part.px(), part.py(), part.pz(), part.energy());
+				double dR = q1.DeltaR(p4part);
+				if (dR < 0.4)
+				{
+					// std::cout << "\t\t addidng " << part.pdgId() << " with energy " << p4part.E() << "\n";
+					quark_1_cone += p4part.E();
+				}
 			}
 		}
 
-		int q1_match = Match(GENPART::q1, genjet);
-		if (q1_match != -1)
+		double quark_2_cone = 0;
+		for (auto const& part: genpart)
 		{
-			// m_matched_jets[static_cast<int>(GENJET::q1)] = genjet[q1_match].clone();
-			std::cout << "\tMatched jet with energy "<< genjet[q1_match].energy() << " to light quark 1 with energy " << m_signal[static_cast<int>(GENPART::q1)]->energy() << "\n";
+			if (part.numberOfDaughters() == 0)
+			{
+				TLorentzVector p4part(part.px(), part.py(), part.pz(), part.energy());
+				double dR = q2.DeltaR(p4part);
+				if (dR < 0.4) quark_2_cone += p4part.E();
+			}				
 		}
+
+		std::cout << "\t quark 1 energy = " << q1.E() << "\n";
+		std::cout << "\t quark 2 energy = " << q2.E() << "\n";
+		std::cout << "\t dR(q1, q2) = " << q1.DeltaR(q2) << "\n";
+		std::cout << "\t W hadronic energy = " << w_had.E() << "\n";
+		std::cout << "\t W leptonic energy = " << w_lep.E() << "\n";
+		std::cout << "\t lepton energy = " << lep.E() << "\n";
+		std::cout << "\t neutrino energy = " << nu.E() << "\n";
+		std::cout << "\t quark_1_cone = " << quark_1_cone << "\n";
+		std::cout << "\t quark_2_cone = " << quark_2_cone << "\n";
+		std::cout << "\t dR(q1, l) = " << q1.DeltaR(lep) << "\n";
+		std::cout << "\t dR(q2, l) = " << q2.DeltaR(lep) << "\n";
+		std::cout << "\t dR(q1, nu) = " << q1.DeltaR(nu) << "\n";
+		std::cout << "\t dR(q2, nu) = " << q2.DeltaR(nu) << "\n";
+		std::cout << "\t dR(q1, b) = " << q1.DeltaR(b) << "\n";
+		std::cout << "\t dR(q1, bbar) = " << q1.DeltaR(bbar) << "\n";
+		std::cout << "\t dR(q2, b) = " << q2.DeltaR(b) << "\n";
+		std::cout << "\t dR(q2, bbar) = " << q2.DeltaR(bbar) << "\n";
+
+		if (q1_match != -1) 
+		{
+			TLorentzVector j1(genjet[q1_match].px(), genjet[q1_match].py(), genjet[q1_match].pz(), genjet[q1_match].energy());
+			std::cout << "\t matched jet 1 energy = " << genjet[q1_match].energy() << ", dR(j1, q1) = " << j1.DeltaR(q1) << "\n";
+			double jet_1_cone = 0;
+			for (auto const& part: genpart)
+			{
+				if (part.numberOfDaughters() == 0)
+				{
+					TLorentzVector p4part(part.px(), part.py(), part.pz(), part.energy());
+					double dR = j1.DeltaR(p4part);
+					if (dR < 0.4)
+					{
+						jet_1_cone += p4part.E();
+					}
+				}
+			}
+			std::cout << "\t dR(j1, l) = " << j1.DeltaR(lep) << ", ";
+			std::cout << "jet_1_cone = " << jet_1_cone << "\n";
+			std::cout << "\t dR(j1, nu) = " << j1.DeltaR(nu) << "\n";
+		}
+		if (q2_match != -1)
+		{
+			TLorentzVector j2(genjet[q2_match].px(), genjet[q2_match].py(), genjet[q2_match].pz(), genjet[q2_match].energy());
+			std::cout << "\t matched jet 2 energy = " << genjet[q2_match].energy() <<  ", dR(j2, q2) = " << j2.DeltaR(q2) << "\n";
+			double jet_2_cone = 0;
+			for (auto const& part: genpart)
+			{
+				if (part.numberOfDaughters() == 0)
+				{
+					TLorentzVector p4part(part.px(), part.py(), part.pz(), part.energy());
+					double dR = j2.DeltaR(p4part);
+					if (dR < 0.4)
+					{
+						jet_2_cone += p4part.E();
+					}
+				}
+			}
+			std::cout << "\t dR(j2, l) = " << j2.DeltaR(lep) << ", ";
+			std::cout << "jet_2_cone = " << jet_2_cone << "\n";
+			std::cout << "\t dR(j2, nu) = " << j2.DeltaR(nu) << "\n";
+		}
+		
+		if (q1_match != -1 && q2_match != -1)
+		{
+			TLorentzVector j2(genjet[q2_match].px(), genjet[q2_match].py(), genjet[q2_match].pz(), genjet[q2_match].energy());
+			TLorentzVector j1(genjet[q1_match].px(), genjet[q1_match].py(), genjet[q1_match].pz(), genjet[q1_match].energy());
+			m_file << q1.E() << ", " 
+				   << q2.E() << ", " 
+				   << q1.DeltaR(q2) << ", " 
+				   << w_had.E() << ", " 
+				   << w_lep.E() << ", " 
+				   << quark_1_cone << ", " 
+				   << quark_2_cone << ", "
+				   << j1.E() << ", "
+				   << j2.E() << "\n";
+		}
+
+		auto stable = FindStableParticles(genpart);
+		std::cout << "\tFound " << stable.size() << " stable particles\n";
 	}
 }
 
@@ -245,21 +381,21 @@ bool AODAnalyzer::FindSignal(reco::GenParticleCollection const& genpart)
 int AODAnalyzer::Match(GENPART target, std::vector<reco::GenJet> const& genjets)
 {
 	int idx = static_cast<int>(target);
-	double t_phi = m_signal[idx]->phi();
-	double t_eta = m_signal[idx]->eta();
+
+	std::cout << "\tgenjet coll size: " << genjets.size() << "\n";
 	
 	TLorentzVector part;
+	part.SetPtEtaPhiM(m_signal[idx]->pt(), m_signal[idx]->eta(), m_signal[idx]->phi(), m_signal[idx]->mass());
 
 	double best_dR = 0.5;
 	int best_match = -1;
 	for (int i = 0; i < static_cast<int>(genjets.size()); ++i)
 	{
-		double j_phi = genjets[i].phi();
-		double j_eta = genjets[i].eta();
-		double deta = t_eta - j_eta;
-		double dphi = (std::abs(t_phi - j_phi) <= PI) ? std::abs(t_phi - j_phi) : std::abs(t_phi - j_phi) - PI;
-		double dR = std::sqrt(deta*deta + dphi*dphi);
-		if (dR < best_dR && dR < 0.4)
+		TLorentzVector jet;
+		jet.SetPtEtaPhiM(genjets[i].pt(), genjets[i].eta(), genjets[i].phi(), genjets[i].mass());
+		double dR = jet.DeltaR(part);
+		// std::cout << "\t jet #" << i << ": E = " << jet.E() << ", dR = " << dR << "\n";
+		if (dR < best_dR && dR < m_dr_thresh)
 		{
 			best_dR = dR;
 			best_match = i;
@@ -267,6 +403,38 @@ int AODAnalyzer::Match(GENPART target, std::vector<reco::GenJet> const& genjets)
 	}
 
 	return best_match;
+}
+
+std::vector<TLorentzVector> AODAnalyzer::FindStableParticles(reco::GenParticleCollection const& genpart)
+{
+	int sz = genpart.size();
+	std::list<TLorentzVector> res;
+	// std::cout << "FindStableParticles: size = " << sz << "\n";
+	for (int i = 0; i < sz; ++i)
+	{
+		int abs_id = std::abs(genpart[i].pdgId()); 
+		// if no daughters or not neutrino
+		if (genpart[i].numberOfDaughters() == 0 && (abs_id != 12 || abs_id != 14 || abs_id != 16))
+		{
+			// std::cout << "Adding " << abs_id << "\n";
+			TLorentzVector p(genpart[i].px(), genpart[i].py(), genpart[i].pz(), genpart[i].energy());
+			res.push_back(p);
+		}
+	}
+	return res;
+}
+
+std::vector<TLorentzVector> AODAnalyzer::ClusterAK4(std::vector<TLorentzVector> stable)
+{
+	std::vector<std::pair<int, double>> dist_to_beam;
+	int sz = stable.size();
+	for (int i = 0; i < sz; ++i)
+	{
+		double d_iB = 1/(stable[i].Pt()*stable[i].Pt());
+		dist_to_beam.push_back({i, d_iB});
+	}
+
+	
 }
 
 //define this as a plug-in
